0f51ce81011b3344da9f4fb40280be8aafa81562
Guilherme Cunha <guilherme.cunha@cromg.org.br>
2019-08-28 16:45:13 -0300

    testes travis ci

--- app/Services/DocumentoService.php
+++ app/Services/DocumentoService.php
@@ -4,7 +4,30 @@
 namespace App\Services;
 
 
+use App\Repositories\DocumentoRepository;
+use App\Validators\DocumentoValidator;
+
 class DocumentoService
 {
+
+    /**
+     * @var DocumentoRepository
+     */
+    private $repository;
+    /**
+     * @var DocumentoValidator
+     */
+    private $validator;
+
+    public function __construct(DocumentoRepository $repository, DocumentoValidator $validator)
+    {
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+
+    public function index()
+    {
+        return $this->repository->paginate(50);
+    }
 
 }
